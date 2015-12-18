3366b5abe48fe2468ccd04e0ba2a2fad879aa4d7
Howard Abrams <howard.abrams@gmail.com>
2015-12-17 19:47:15 -0800

    Fixed some spelling

--- elisp/cider-always-eval.el
+++ elisp/cider-always-eval.el
@@ -1,4 +1,4 @@
-;;; CIDER-ALWAYS-EVAL --- Summary
+;;; CIDER-ALWAYS-EVAL --- Evaluates expressions whenever function changes
 ;;
 ;; Author: Howard Abrams <howard.abrams@gmail.com>
 ;; Copyright © 2015, Howard Abrams, all rights reserved.
@@ -43,13 +43,21 @@
 ;;  function using the `cider-eval-defun-at-point', the results will
 ;;  be displayed.
 ;;
+;;  Trigger more by advising other functions:
+;;
+;;      (advice-add 'cider-eval-region :after #'cider-eval-expression-collection)
+;;
+;;  Note: Do NOT advice `cider-eval-last-sexp' as that is being used
+;;  for evaluation. Need to change that.
+;;
+;;
 ;;  The Backstory: Frank and I were at the PDX Emacs Hackers meetup,
 ;;  and he mentioned that he liked this feature in Lighttable, where
 ;;  you could set up a buffer of expressions, and then when you
 ;;  modified the function, it would re-evaluate them automatically.
 ;;
-;;  So, without actually seeing Lighttable do this, I figured I would
-;;  build something similar. This is really a priliminary stab at the
+;;  So, without actually seeing Light Table do this, I figured I would
+;;  build something similar. This is really a preliminary stab at the
 ;;  process, as the code calls `cider-eval-last-sexp', but it really
 ;;  should be calling `cider-interactive-eval'.
 ;;
@@ -81,7 +89,7 @@
 ;;; Code:
 
 (defcustom cider-expression-collection-buffer-name "*cider-expressions*"
-  "Buffer name used for Clojure expressions to be evaluated each time a function is redefined."
+  "Buffer name for Clojure expressions to be evaluated when functions are redefined."
   :group 'cider)
 
 (defun cider-create-expression-collection-buffer ()
@@ -91,6 +99,7 @@
   (when (= (point-min) (point-max))
     (insert ";; Cider Expressions Evaluator:
 ;;   Evaluates every top-level expression in this buffer.
+;;   Deletes comments following expressions, so place each on their own line.
 
 ")
     (clojure-mode 1)))
@@ -100,9 +109,11 @@
   (forward-sexp)
   (when (not (= (point) (line-end-position)))
     (kill-line))
-  (cider-eval-last-sexp 4) ; Evaluate and insert into buffer
-  (insert " ; => ")        ; Turn the evaluation into a comment
-  (sit-for .1)             ; Wait for results... hrm... Don't likey
+  (cider-eval-last-sexp 4)    ; Evaluate and insert into buffer
+  (insert " ; => ")           ; Turn the evaluation into a comment
+                                        ; TODO: Call `cider-interactive-eval'.
+  (sit-for .1)                ; Wait for results... hrm... Don't likey
+                                        ; TODO: Wait until completed
   (end-of-line))
 
 (defun cider-eval-expression-collection ()
