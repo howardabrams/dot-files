fef57b68d94b3db02515ceba90193bd78efa1d96
Howard Abrams <howard.abrams@gmail.com>
2015-12-30 12:05:18 -0800

    Cleaning up the warnings with commentary
    This code I took from Mickey Petersen really should be a project, as it
    has been useful.

--- elisp/smart-scan.el
+++ elisp/smart-scan.el
@@ -1,27 +1,62 @@
+;;; SMART-SCAN --- Jump to next matching thing at point
+;;
+;; Author: `(concat user-full-name)` <`(concat user-mail-address)`>
+;; Copyright © `(format-time-string "%Y")`, `(concat user-full-name)`, all rights reserved.
+;; Created: `(format-time-string "%e %B %Y")`
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
 ;; Taken from Mastering Emacs blog site
 ;; http://www.masteringemacs.org/articles/2011/01/14/effective-editing-movement/
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
 
 (defvar smart-use-extended-syntax nil
-  "If t the smart symbol functionality will consider extended
-syntax in finding matches, if such matches exist.")
- 
+  "If t the smart symbol functionality will consider extended syntax in finding matches, if such matches exist.")
+
 (defvar smart-last-symbol-name ""
   "Contains the current symbol name.
- 
+
 This is only refreshed when `last-command' does not contain
 either `smart-symbol-go-forward' or `smart-symbol-go-backward'")
- 
+
 (make-local-variable 'smart-use-extended-syntax)
- 
+
 (defvar smart-symbol-old-pt nil
-  "Contains the location of the old point")
- 
+  "Contains the location of the old point.")
+
 (defun smart-symbol-goto (name direction)
   "Jumps to the next NAME in DIRECTION in the current buffer.
- 
+
 DIRECTION must be either `forward' or `backward'; no other option
 is valid."
- 
+
   ;; if `last-command' did not contain
   ;; `smart-symbol-go-forward/backward' then we assume it's a
   ;; brand-new command and we re-set the search term.
@@ -44,21 +79,22 @@ is valid."
                              (syntax-ppss (point))) '(string comment))
                 (throw 'done t))))
     (goto-char smart-symbol-old-pt)))
- 
+
 (defun smart-symbol-go-forward ()
-  "Jumps forward to the next symbol at point"
+  "Jump forward to the next symbol at point."
   (interactive)
   (smart-symbol-goto (smart-symbol-at-pt 'end) 'forward))
- 
+
 (defun smart-symbol-go-backward ()
-  "Jumps backward to the previous symbol at point"
+  "Jump backward to the previous symbol at point."
   (interactive)
   (smart-symbol-goto (smart-symbol-at-pt 'beginning) 'backward))
- 
+
 (defun smart-symbol-at-pt (&optional dir)
-  "Returns the symbol at point and moves point to DIR (either `beginning' or `end') of the symbol.
- 
-If `smart-use-extended-syntax' is t then that symbol is returned
+  "Return symbol at point and move point to DIR.
+
+Either `beginning' or `end') of the symbol.  If
+`smart-use-extended-syntax' is t then that symbol is returned
 instead."
   (with-syntax-table (make-syntax-table)
     (if smart-use-extended-syntax
@@ -76,6 +112,9 @@ instead."
              (t (error "Invalid direction")))
             word)
         (error "No symbol found")))))
- 
+
 (global-set-key (kbd "M-n") 'smart-symbol-go-forward)
 (global-set-key (kbd "M-p") 'smart-symbol-go-backward)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; smart-scan.el ends here
